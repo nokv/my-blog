117d895527e33374c526e508c4ec0855ae791b0a
nokl <naoki.tkb@gmail.com>
2021-03-09 17:42:17 +0900

    update: styles

--- src/components/common/TheHeader.vue
+++ src/components/common/TheHeader.vue
@@ -3,20 +3,20 @@
         <div class="header__inner">
             <aside class="header__aside">
                 <h1 class="header__title">
-                    <nuxt-link to="/" class="header__title-link">Naok Takahashi blog</nuxt-link>
+                    <nuxt-link :to="Routes.HOME" class="header__title-link">Naok Takahashi blog</nuxt-link>
                 </h1>
             </aside>
             <nav class="header__nav">
                 <ul class="header__link-list">
                     <li class="header__link-item">
-                        <nuxt-link :to="Routes.HOME" class="header__link">top</nuxt-link>
+                        <nuxt-link :to="Routes.HOME" class="header__link">posts</nuxt-link>
                     </li>
                     <li class="header__link-item">
                         <nuxt-link :to="Routes.ABOUT" class="header__link">about</nuxt-link>
                     </li>
-                    <li class="header__link-item">
+                    <!-- <li class="header__link-item">
                         <nuxt-link :to="Routes.POST_SEARCH" class="header__link">search</nuxt-link>
-                    </li>
+                    </li> -->
                 </ul>
             </nav>
         </div>
---
+++ src/pages/about/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="p-about">
+        <AppHeadingH1>About</AppHeadingH1>
+        <section class="about-me">
+            <p>I am a front-end developer based in Tokyo, Japan.</p>
+        </section>
+        <section class="sns">
+            <AppHeadingH2>SNS</AppHeadingH2>
+            <ul class="sns__list">
+                <li class="sns__item">
+                    <p class="sns__name">GitHub:</p>
+                    <a class="sns__link" href="http://github.com/nokv" target="_blank" rel="noopener noreferrer"
+                        >@nokv</a
+                    >
+                </li>
+            </ul>
+        </section>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import AppHeadingH1 from '~/components/common/AppHeadingH1.vue';
+import AppHeadingH2 from '~/components/common/AppHeadingH2.vue';
+
+export default Vue.extend({
+    components: {
+        AppHeadingH1,
+        AppHeadingH2,
+    },
+    data(): {} {
+        return {};
+    },
+    head() {
+        return {
+            titleTemplate: 'ABOUT',
+        };
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/styles/pages/about';
+</style>
