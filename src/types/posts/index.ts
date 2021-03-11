406e6517b40245a7d854a34af565a3e8a380c754
nokl <naoki.tkb@gmail.com>
2021-03-11 12:27:31 +0900

    refactor: type

--- src/types/posts/index.ts
+++ src/types/posts/index.ts
@@ -1,8 +1,6 @@
 import { IContentDocument } from '@nuxt/content/types/content';
-
-export interface PostMeta {
-    title: string;
-    date: string;
+export interface PostType extends IContentDocument {
+    readonly title: string;
+    readonly date: string;
+    readonly description: string;
 }
-
-export type PostType = IContentDocument & PostMeta;
